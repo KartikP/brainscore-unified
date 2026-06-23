3f363be6292264e9c990a68cbd1e7e8d601323e2
Kartik Pradeepan <kartikspradeepan@gmail.com>
2026-06-23 12:57:41 -0400

    Migrate gpt2 and chance_baseline to the deferred-factory registration form

--- brainscore/models/chance_baseline/__init__.py
+++ brainscore/models/chance_baseline/__init__.py
@@ -1,4 +1,9 @@
 from brainscore import model_registry
-from .model import get_model
 
-model_registry['chance-baseline'] = lambda: get_model('chance-baseline')
+
+def _load():  # defer heavy imports to load time (keep `import brainscore` light)
+    from .model import get_model
+    return get_model('chance-baseline')
+
+
+model_registry['chance-baseline'] = _load
--- brainscore/models/gpt2/__init__.py
+++ brainscore/models/gpt2/__init__.py
@@ -1,4 +1,9 @@
 from brainscore import model_registry
-from .model import get_model
 
-model_registry['gpt2'] = lambda: get_model('gpt2')
+
+def _load():  # defer heavy imports to load time (keep `import brainscore` light)
+    from .model import get_model
+    return get_model('gpt2')
+
+
+model_registry['gpt2'] = _load
